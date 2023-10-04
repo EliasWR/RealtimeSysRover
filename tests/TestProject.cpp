b0277c359fdba622a7faab1b12f4a804b66db4ae
elias.woie <elias.woie@kongsberg.com>
2023-10-04 21:12:03 +0200

    Added initial git workflows and testing framework

--- tests/TestProject.cpp
+++ tests/TestProject.cpp
@@ -1,6 +1,5 @@
-#define CATCH_CONFIG_MAIN
-#include <iostream>
-#include <catch2/catch_test_macros.hpp>
+#include <Catch2/catch_all.hpp>
+//#include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("TestTest") {
 bool result = true;
