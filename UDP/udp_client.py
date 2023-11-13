cea77fb66ae01c46b9b583619480b720a9b59a77
elias.woie <elias.woie@kongsberg.com>
2023-11-13 12:15:10 +0100

    Cleanup python file

--- UDP/udp_client.py
+++ UDP/udp_client.py
@@ -1,12 +1,11 @@
 import socket
 import cv2
-from protobuf.my_messages_pb2 import VideoFeed, Instruction  # Generated from protoc
+from protobuf.my_messages_pb2 import VideoFeed, Instruction
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('127.0.0.1', 8080)
-MAX_UDP_PACKET_SIZE = 65507  # Maximum size of a UDP packet
+MAX_UDP_PACKET_SIZE = 65507
 
-# Initialize webcam
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -14,22 +13,19 @@ if not cap.isOpened():
     exit()
 
 while True:
-    ret, frame = cap.read()  # Read a frame from the webcam
+    ret, frame = cap.read()
 
     if not ret:
         print("Failed to grab frame!")
         break
 
-    # Display the frame for debugging purposes
     # cv2.imshow("Webcam", frame)
 
-    # Reduce resolution until it fits in UDP packet
     scale_factor = 1.0
     while True:
-        # Resize frame with the current scaling factor
         dim = (int(frame.shape[1] * scale_factor), int(frame.shape[0] * scale_factor))
         resized_frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-        # Encode frame to JPEG format
+
         is_success, buffer = cv2.imencode(".jpg", resized_frame)
 
         if not is_success:
@@ -54,14 +50,11 @@ while True:
     instruction = Instruction()
     instruction.ParseFromString(serialized_instruction)
 
-    # Print received instruction - execute it if applicable
     print("Received instruction:", instruction.messageInstruction)
 
-    # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
 sock.close()
