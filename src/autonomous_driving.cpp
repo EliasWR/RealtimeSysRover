20d154aaf5d9dcb85f873c9a5b96219183759b82
elias.woie <elias.woie@kongsberg.com>
2023-12-04 09:01:45 +0100

    Fixed printing

--- src/autonomous_driving.cpp
+++ src/autonomous_driving.cpp
@@ -124,13 +124,15 @@ std::pair<int, int> AutonomousDriving::interpretLatestDetection (Detection& dete
             bool isCentered =   std::abs(boxCenterX) < detection.frameSize.first/2 + centerThreshold * detection.frameSize.first &&
                                 std::abs(boxCenterX) > detection.frameSize.first/2 - centerThreshold * detection.frameSize.first;
 
+            bool isSizeInRange = box.width >= detection.frameSize.first * sizeMinThreshold &&
+                                 box.width <= detection.frameSize.first * sizeMaxThreshold;
 
             // std::cout << "boxCenterX: " << boxCenterX << "Threshold: " << detection.frameSize.first/2 - centerThreshold << " , " << detection.frameSize.first/2 + centerThreshold << std::endl;
             // std::cout << "Is centered: " << isCentered << std::endl;
-            bool isSizeInRange = box.width >= detection.frameSize.first * sizeMinThreshold &&
-                                 box.width <= detection.frameSize.first * sizeMaxThreshold;//  &&
-                                 // box.height >= detection.frameSize.second * sizeMinThreshold &&
-                                 // box.height <= detection.frameSize.second * sizeMaxThreshold;
+            // std::cout << "Is size in range: " << isSizeInRange << std::endl;
+            // std::cout << "box.width: " << box.width << "Threshold: " << detection.frameSize.first * sizeMinThreshold << " , " << detection.frameSize.first * sizeMaxThreshold << std::endl;
+            // box.height >= detection.frameSize.second * sizeMinThreshold &&
+            // box.height <= detection.frameSize.second * sizeMaxThreshold;
 
             // Controlling joystick
             int displacementX = detection.frameSize.first/2 - boxCenterX;
